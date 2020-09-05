de4a1218af72d29f7a2c453525c484dc9259bbf2
MapMaths <62785981+MapMaths@users.noreply.github.com>
2020-09-05 10:23:38 +0800

    commit by vscode

--- nav.js
+++ nav.js
@@ -1,6 +1,17 @@
 var obj;
-obj = document;
-obj.write("<nav id='navjs-nav' />");
+
+obj = document.getElementById("to-navjs");
+obj.innerHTML = "<nav id='navjs-nav'></nav>";
+
 obj = document.getElementById("navjs-nav");
-obj.style.backgroundColor = black;
+obj.style.backgroundColor = "black";
 obj.style.position = "fixed";
+obj.innerHTML = "<a id='navjs-nav-address-for-pfp'></a>"
+
+obj = document.getElementById("navjs-nav-address-for-pfp");
+obj.setAttribute("href", "/");
+obj.innerHTML = "<img src='favicon.ico' width='55' height='60' id='navjs-nav-address-img-for-pfp' />"
+
+obj = document.getElementById("navjs-nav-address-img-for-pfp");
+obj.style.borderRadius = "50%";
+obj.style.marginTop = "1em";
