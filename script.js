e6a224621b416fc75492650359cc441cd7d56e2a
MapMaths <62785981+MapMaths@users.noreply.github.com>
2021-12-06 19:52:05 +0800

    🧪 Updated scroll effects

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 var mm = document.querySelector('nav p strong');
 var mmtop = mm.getBoundingClientRect().top;
 var mmleft = mm.getBoundingClientRect().left;
+var mmn = document.querySelector('nav table tbody tr td:nth-of-type(1)')
 
 var gh = document.getElementById("gh");
 gh.onclick = () => {
@@ -14,24 +15,26 @@ gl.onclick = () => {
     document.location.href = "https://glitch.com/@MapMaths"
 };
 
+mmn.style.top = mmtop + "px";
+mmn.style.left = mmleft + "px";
+mmn.style.fontSize = "4rem";
+
 window.addEventListener('scroll', () => {
     if (window.scrollY <= window.innerHeight) {
         document.querySelector('nav').style.background =
             "linear-gradient(" +
             (window.scrollY / window.innerHeight + 1) * 45 +
             "deg, tomato, yellow)";
-        mm.style.transform =
-            "translateX(-" +
-            window.scrollY / (mmleft - document.querySelector('nav').getBoundingClientRect().left) + "px)";
         document.querySelector('tr').style.background = "none";
         document.querySelector('tr').style.boxShadow = "none";
-        //document.querySelector('nav table tbody tr td strong').style.visibility = "hidden";
+        mmn.style.top = mmtop - window.scrollY / window.innerHeight * mmtop + "px";
+        mmn.style.left = mmleft - window.scrollY / window.innerHeight * mmleft + 1 + "px";
+        mmn.style.fontSize = 4 - window.scrollY / window.innerHeight * 2.7 + "rem";
     } else {
         document.querySelector('tr').style.background = "linear-gradient(90deg, tomato, yellow)";
         document.querySelector('tr').style.boxShadow = "2px 2px 2px #11111122";
-        //document.querySelector('nav table tbody tr td strong').style.visibility = "visible";
+        mmn.style.top = "0px";
+        mmn.style.left = "1px";
+        mmn.style.fontSize = "1.3rem";
     }
 });
-
-console.log('%c            \n            \n  MapMaths  \n            \n            ',
-            'font-size:4.16666667vw; color:#A89E84; background:#FDF6E3;');
